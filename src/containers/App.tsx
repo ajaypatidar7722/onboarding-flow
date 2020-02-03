22f3cb77c82249b58e53ee590a38e9d77a8f6b80
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2020-02-03 18:20:38 +0530

    [Fix] container/App.tsx

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../routes';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
