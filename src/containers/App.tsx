f0600a5cccb9af5f174979a768a4b34f315ebe92
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2020-02-03 20:06:46 +0530

    [Fix] containers/App.tsx

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../routes';
