784f8a76dcffc10ef3b836341574cd65c0d82475
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2020-02-03 20:07:48 +0530

    [Fix] src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
